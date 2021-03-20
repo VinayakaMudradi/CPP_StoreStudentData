4dbcbdd9f1d5d862ee39c3c9b9ffb347bb9e16a0
Vinayaka Naik <vinayakamudradi@gmail.com>
2021-03-20 21:07:21 +0530

    Two function call added

--- main.c
+++ main.c
@@ -4,4 +4,11 @@ typedef struct Student{
     int age;
     int rollno;
     char collegeName[50]
-};
+}studentData;
+
+int main(){
+    int numberOfStudent = 0;
+    studentData* pointToDymanicMemory = NULL;
+    getTheData(&numberOfStudent);
+    createTheDynamicMemory(&numberOfStudent);
+}
